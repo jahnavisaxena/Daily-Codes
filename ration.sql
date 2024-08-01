3dfbd5a4733d1713608f163a8a06447d6606edb8
Jahnavi Saxena <149791017+jahnavisaxena@users.noreply.github.com>
2024-08-01 12:26:02 +0530

    Create ration.sql

---
+++ ration.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE home;
+
+USE home;
+
+CREATE TABLE ration(
+srno INT PRIMARY KEY,
+food_item VARCHAR(50),
+cost INT NOT NULL,
+quantity INT
+);
+
+INSERT INTO ration 
+(srno,food_item,cost,quantity)
+VALUES
+(1,"Flour",500,1),
+(2,"rice",200,2),
+(3,"vegetables",70,12),
+(4,"chips",30,5),
+(5,"biscuits",20,6);
+
+SELECT * FROM ration;
